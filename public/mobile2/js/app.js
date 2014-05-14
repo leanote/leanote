ab5e5c7b769268068f946984792d1e49a71c06ff
life <lifephp@gmail.com>
2014-05-14 22:23:35 +0800

    angularjs mobile v0.1

---
+++ public/mobile2/js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function log(o) {
+	console.log(o);
+}
+
+angular.module('myApp', [
+    'ngTouch',
+    'ngRoute',
+    'ngAnimate',
+    'myApp.controllers',
+    'myApp.memoryServices'
+]).
+config(['$routeProvider', '$interpolateProvider', function ($routeProvider, $interpolateProvider) {
+	
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
+	
+    $routeProvider.when('/notebooks', {templateUrl: '/public/mobile2/tpl/notebooks.html', controller: 'NotebookListCtrl'});
+    $routeProvider.when('/notes/:notebookId', {templateUrl: '/public/mobile2/tpl/notes.html', controller: 'NoteListCtrl'});
+    $routeProvider.when('/note/:noteId', {templateUrl: '/public/mobile2/tpl/note.html', controller: 'NoteCtrl'});
+    $routeProvider.otherwise({redirectTo: '/notebooks'});
+}]);
+
