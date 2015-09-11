7167705409c13aa34bcb72f54337de4edbb6ed85
lealife <lifephp@gmail.com>
2015-09-11 18:31:37 +0800

    add db/auth test

---
+++ tests/auth_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"testing"
+	"github.com/leanote/leanote/app/db"
+//	. "github.com/leanote/leanote/app/lea"
+	"github.com/leanote/leanote/app/service"
+//	"gopkg.in/mgo.v2"
+//	"fmt"
+)
+
+func init() {
+	db.Init("mongodb://localhost:27017/leanote", "leanote")
+	service.InitService()
+}
+
+// 测试登录
+func TestAuth(t *testing.T) {
+	_, err := service.AuthS.Login("admin", "abc123")
+	if err != nil {
+		t.Error("Admin User Auth Error")
+	}
+}
