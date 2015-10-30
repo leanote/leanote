1844513f08c8c3ec781ff0a9e7df3db3f3c2a5d7
lealife <lifephp@gmail.com>
2015-10-30 13:52:50 +0800

    mac下cmd+shift+c 不能调出toggle code
    https://github.com/leanote/leanote/issues/248

--- public/tinymce/plugins/leanote_code/plugin.js
+++ public/tinymce/plugins/leanote_code/plugin.js
@@ -50,6 +50,9 @@ tinymce.PluginManager.add('leanote_code', function(editor, url) {
 
 	// brush 刷子
 	function toggleCode(brush) {
+		if (LEA.readOnly) {
+			return;
+		}
 		ed = tinymce.activeEditor;
 		var node = ed.selection.getNode();
 
@@ -331,7 +334,7 @@ tinymce.PluginManager.add('leanote_code', function(editor, url) {
 	ed.addCommand('toggleCode', toggleCode);
     
     ed.addShortcut('ctrl+shift+c', '', 'toggleCode');
-	ed.addShortcut('command+shift+c', '', 'toggleCode');
+	ed.addShortcut('meta+shift+c', '', 'toggleCode');
 
 	// life
 	if(LeaAce.canAce()) {
